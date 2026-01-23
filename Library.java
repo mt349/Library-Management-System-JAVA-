d6505067255ac3c672d08eca0537965815550cca
Mahnoor Tariq <mahnoort313@gmail.com>
2026-01-23 04:18:20 -0800

    Add files via upload

---
+++ Library.java
@@ -0,0 +1,116 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+class Book{
+    int bookId;
+    String title;
+    String author;
+    boolean isIssued;
+
+    Book(int bookId,String title,String author) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.isIssued = false;
+    }
+
+    void displayBook() {
+        System.out.println( bookId + " | " + title + " | " + author + (isIssued ? "Issued" : "Available"));
+    }
+}
+
+class Library{
+    ArrayList<Book> books = new ArrayList<>();
+
+    void addBook(Book book) {
+        books.add(book);
+        System.out.println("Book added Sucessfully!");
+  }
+
+  void viewBooks() {
+    if(books.isEmpty()) {
+        System.out.println("No books found!");
+        return;
+    }
+    for (Book book : books) {
+        book.displayBook();
+    }
+  }
+
+  void issueBook(int bookId) {
+    for(Book book : books) {
+        if(book.bookId == bookId && !book.isIssued) {
+            book.isIssued = true;
+            System.out.println("Book issued Sucessfully!");
+            return;
+        }
+    }
+  System.out.println("Book is not available.");
+}
+     void returnBook(int bookId) {
+        for (Book book : books) {
+            if (book.bookId == bookId && book.isIssued) {
+                book.isIssued = false;
+                System.out.println("Book returned Sucessfully!");
+                return;
+            }
+        }
+        System.out.println("Incorrect Book ID!");
+     }
+}
+
+class Main{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Library library = new Library();
+        while(true) {
+        System.out.println("----LIBRARY MANAGEMENT SYSTEM----");
+        System.out.println("1. Add Books :");
+        System.out.println("2. View Books :");
+        System.out.println("3. Issue Book :");
+        System.out.println("4. Return Book :");
+        System.out.println("5. Exit ");
+
+        System.out.println("Enter your choice :");
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+
+        switch(choice) {
+            case 1 :
+                System.out.println("Enter Book ID :");
+                int bookId = scanner.nextInt();
+                scanner.nextLine();
+                System.out.println("Enter title :");
+                String title = scanner.nextLine();
+                System.out.println("Enter author :");
+                String author = scanner.nextLine();
+                library.addBook(new Book(bookId,title,author));
+                break;
+
+                case 2 :
+                    library.viewBooks();
+
+                    case 3 :
+                        System.out.println("Enter Book ID to issue :");
+                        library.issueBook(scanner.nextInt());
+
+                        case 4 :
+                            System.out.println("Enter Book ID to return :");
+                            library.returnBook(scanner.nextInt());
+                            break;
+
+                            case 5 :
+                                System.out.println("Exiting........");
+                                System.exit(0);
+
+                                default :
+                                System.out.println("Invalid Choice try again!");
+                                break;
+                   
+        }
+
+        
+
+        
+    } }
+}
